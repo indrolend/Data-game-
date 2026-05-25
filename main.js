d0a1fbc89653479b81839082b4abe1562804c5d2
indrolend <indrolend@gmail.com>
2026-05-25 01:22:22 -0500

    Initial commit: Data(game) structure refactor

---
+++ main.js
@@ -0,0 +1,4 @@
+import * as THREE from "three";
+import { RectAreaLightUniformsLib } from "three/addons/lights/RectAreaLightUniformsLib.js";
+
+// TODO: Paste all gameplay logic here from the previous <script type="module"> block if available.
